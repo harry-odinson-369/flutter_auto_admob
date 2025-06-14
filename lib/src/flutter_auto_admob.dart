49ebddc7251754a1d539c98fdddfd35f2f630023
Harry Odinson <vandet.dev@gmail.com>
2025-06-14 15:10:23 +0700

    update v0.0.4

--- lib/src/flutter_auto_admob.dart
+++ lib/src/flutter_auto_admob.dart
@@ -4,59 +4,52 @@ import 'dart:developer';
 import 'package:flutter_auto_admob/src/config.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 
-AutoAdmobConfig _config = AutoAdmobConfig();
-bool _isInitialized = false;
-
-bool _isInterstitialAdCoolingDown = true;
-bool _isAppOpenAdCoolingDown = true;
-
-InterstitialAd? _interstitialAd;
-AppOpenAd? _appOpenAd;
-
-Timer? _interstitialAdTimer;
-Timer? _appOpenAdTimer;
-
-Completer? _interstitialAdCompleter;
-Completer? _appOpenAdCompleter;
-
 class AutoAdmob {
-  static AutoAdmobConfig get config => _config;
-  static set config(AutoAdmobConfig config) {
-    _config = config;
-  }
+  AutoAdmobConfig config = AutoAdmobConfig();
+  bool _isInitialized = false;
 
-  static void onAppLifeCycleStateChanged(
-    void Function(AppState state) stateChanged,
-  ) {
+  bool _isInterstitialAdCoolingDown = true;
+  bool _isAppOpenAdCoolingDown = true;
+
+  InterstitialAd? _interstitialAd;
+  AppOpenAd? _appOpenAd;
+
+  Timer? _interstitialAdTimer;
+  Timer? _appOpenAdTimer;
+
+  Completer? _interstitialAdCompleter;
+  Completer? _appOpenAdCompleter;
+
+  void onAppLifeCycleStateChanged(void Function(AppState state) stateChanged) {
     AppStateEventNotifier.startListening();
     AppStateEventNotifier.appStateStream.forEach(stateChanged);
     log("[AUTO ADMOB] started listening app life cycle state.");
   }
 
-  static Future initialize({AutoAdmobConfig? config}) async {
-    if (config != null) _config = config;
+  Future initialize({AutoAdmobConfig? config}) async {
+    if (config != null) this.config = config;
     assert(
-      _config.interstitialCooldown.inSeconds >= 60,
+      this.config.interstitialCooldown.inSeconds >= 60,
       "[AUTO ADMOB] the interstitial ad cool down should be equal or greater than 1 minute.",
     );
     assert(
-      _config.appOpenAdCooldown.inSeconds >= 60,
+      this.config.appOpenAdCooldown.inSeconds >= 60,
       "[AUTO ADMOB] the app open ad cool down should be equal or greater than 1 minute.",
     );
     await MobileAds.instance.initialize();
     _isInitialized = true;
     _interstitialAdTimer = Timer.periodic(
-      _config.calculatedInterstitialAdCooldown,
+      this.config.calculatedInterstitialAdCooldown,
       _onInterstitialAdTimerExecuted,
     );
     _appOpenAdTimer = Timer.periodic(
-      _config.calculatedAppOpenAdCooldown,
+      this.config.calculatedAppOpenAdCooldown,
       _onAppOpenAdTimerExecuted,
     );
   }
 
   /// Cancel all auto ad and will require to call initialize again.
-  static Future destroy() async {
+  Future destroy() async {
     _interstitialAdTimer?.cancel();
     _appOpenAdTimer?.cancel();
     _interstitialAdTimer = null;
@@ -70,39 +63,39 @@ class AutoAdmob {
     _isInitialized = false;
   }
 
-  static void _pauseAppOpenAd() {
+  void _pauseAppOpenAd() {
     _isAppOpenAdCoolingDown = true;
     _appOpenAdTimer?.cancel();
     _appOpenAdTimer = null;
   }
 
-  static void _resumeAppOpenAd() {
-    Future.delayed(_config.delayBetween, () {
+  void _resumeAppOpenAd() {
+    Future.delayed(config.delayBetween, () {
       _isAppOpenAdCoolingDown = false;
       _appOpenAdTimer = Timer.periodic(
-        _config.calculatedAppOpenAdCooldown,
+        config.calculatedAppOpenAdCooldown,
         _onAppOpenAdTimerExecuted,
       );
     });
   }
 
-  static void _pauseInterstitialAd() {
+  void _pauseInterstitialAd() {
     _isInterstitialAdCoolingDown = true;
     _interstitialAdTimer?.cancel();
     _interstitialAdTimer = null;
   }
 
-  static void _resumeInterstitialAd() {
-    Future.delayed(_config.delayBetween, () {
+  void _resumeInterstitialAd() {
+    Future.delayed(config.delayBetween, () {
       _isInterstitialAdCoolingDown = false;
       _interstitialAdTimer = Timer.periodic(
-        _config.calculatedInterstitialAdCooldown,
+        config.calculatedInterstitialAdCooldown,
         _onInterstitialAdTimerExecuted,
       );
     });
   }
 
-  static void _createAppOpenCompleter() {
+  void _createAppOpenCompleter() {
     if (_appOpenAdCompleter?.isCompleted == false) {
       _appOpenAdCompleter?.complete();
     }
@@ -110,13 +103,13 @@ class AutoAdmob {
     _appOpenAdCompleter = Completer();
   }
 
-  static void _completeAppOpenAd() {
+  void _completeAppOpenAd() {
     if (_appOpenAdCompleter?.isCompleted == false) {
       _appOpenAdCompleter?.complete();
     }
   }
 
-  static void _createInterstitialAdCompleter() {
+  void _createInterstitialAdCompleter() {
     if (_interstitialAdCompleter?.isCompleted == false) {
       _interstitialAdCompleter?.complete();
     }
@@ -124,20 +117,20 @@ class AutoAdmob {
     _interstitialAdCompleter = Completer();
   }
 
-  static void _completeInterstitialAd() {
+  void _completeInterstitialAd() {
     if (_interstitialAdCompleter?.isCompleted == false) {
       _interstitialAdCompleter?.complete();
     }
   }
 
-  static void _onInterstitialAdTimerExecuted(Timer timer) {
-    if (_config.interstitialAdLoadType == AutoAdmobLoadType.none) {
+  void _onInterstitialAdTimerExecuted(Timer timer) {
+    if (config.interstitialAdLoadType == AutoAdmobLoadType.none) {
       _isInterstitialAdCoolingDown = false;
     } else {
       if (_interstitialAd == null) {
         _createInterstitialAdCompleter();
         InterstitialAd.load(
-          adUnitId: _config.interstitialAdUnitId,
+          adUnitId: config.interstitialAdUnitId,
           request: AdRequest(),
           adLoadCallback: InterstitialAdLoadCallback(
             onAdLoaded: (ad) {
@@ -171,14 +164,14 @@ class AutoAdmob {
     }
   }
 
-  static void _onAppOpenAdTimerExecuted(Timer timer) {
-    if (_config.appOpenAdLoadType == AutoAdmobLoadType.none) {
+  void _onAppOpenAdTimerExecuted(Timer timer) {
+    if (config.appOpenAdLoadType == AutoAdmobLoadType.none) {
       _isAppOpenAdCoolingDown = false;
     } else {
       if (_appOpenAd == null) {
         _createAppOpenCompleter();
         AppOpenAd.load(
-          adUnitId: _config.appOpenAdUnitId,
+          adUnitId: config.appOpenAdUnitId,
           request: AdRequest(),
           adLoadCallback: AppOpenAdLoadCallback(
             onAdLoaded: (ad) {
@@ -211,7 +204,7 @@ class AutoAdmob {
     }
   }
 
-  static Future showInterstitialAd({
+  Future showInterstitialAd({
     AdRequest? request,
     InterstitialAdLoadCallback? callback,
     bool waitUntil = true,
@@ -254,7 +247,7 @@ class AutoAdmob {
     if (waitUntil) return _interstitialAdCompleter?.future;
   }
 
-  static Future showAppOpenAd({
+  Future showAppOpenAd({
     AdRequest? request,
     AppOpenAdLoadCallback? callback,
     bool waitUntil = true,
